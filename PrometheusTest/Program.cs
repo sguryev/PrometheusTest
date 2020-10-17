8e95d5e9e4a0e6e0859ff1b3780be15431804829
Sergey G <guryev.s@gmail.com>
2020-10-17 22:30:46 +0300

    Initial commit

---
+++ PrometheusTest/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PrometheusTest
+{
+    using System;
+    using System.Threading.Tasks;
+    using Prometheus;
+
+    public class Program
+    {
+        private static readonly Counter TickTock =
+            Metrics.CreateCounter("sampleapp_ticks_total", "Just keeps on ticking...");
+        
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting Prometheus metrics.");
+
+            var server = new MetricServer("localhost", 9090);
+            server.Start();
+            
+            Console.WriteLine("Starting endless loop.");
+            
+            while (true)
+            {
+                TickTock.Inc();
+                Console.WriteLine($"TickTock: {TickTock.Value}.");
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
---
+++ PrometheusTestWeb/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PrometheusTestWeb
+{
+    using Prometheus;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            
+            app.UseMetricServer();
+            
+            app.Use((context, next) =>
+            {
+                // Http Context
+                var counter = Metrics.CreateCounter
+                ("PathCounter", "Count request", 
+                    new CounterConfiguration{
+                        LabelNames = new [] {"method", "endpoint"}
+                    }); 
+                // method: GET, POST etc.
+                // endpoint: Requested path
+                counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+                return next();
+            });
+
+            app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
+        }
+    }
+}
